07775e68b4f7972f2133d5da1ef4372017cf0a6e
Appau Ernest K.M <kappernie@users.noreply.github.com>
2019-12-24 13:17:03 +0000

    changed hello to hey there

--- bikeshare.py
+++ bikeshare.py
@@ -18,7 +18,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hey there ! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). 
     city = input("Hi,please enter the name of the city bike share data you want to explore__options[Chicago,new york city ,washington]:: ").lower()
 
